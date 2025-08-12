86fa99c4bf4648bb0c8ad3e255e8d77c2df20f7d
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-08-12 15:40:16 +0300

    add dashboard.html

---
+++ src/main/java/com/example/hoteluiservice/controller/DashboardController.java
@@ -0,0 +1,32 @@
+package com.example.hoteluiservice.controller;
+
+import com.example.hoteluiservice.dto.UserDto;
+import com.example.hoteluiservice.service.UserService;
+import com.example.hoteluiservice.util.CurrentUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+
+    private final UserService userService;
+
+
+    @GetMapping
+    public String dashboard(@CurrentUser UserDto user, Model model) {
+        // Пользователь автоматически передается через аннотацию
+        model.addAttribute("user", user);
+
+        // Можете добавить дополнительную логику
+        // model.addAttribute("bookings", bookingService.getUserBookings(user.getId()));
+        // model.addAttribute("favorites", favoriteService.getUserFavorites(user.getId()));
+
+        return "dashboard";
+    }
+}
