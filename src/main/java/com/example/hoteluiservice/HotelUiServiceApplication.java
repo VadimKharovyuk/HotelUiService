8896fb3bf8acf61626b26d12e5b24ede644a6405
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-08-12 03:40:24 +0300

    done

--- src/main/java/com/example/hoteluiservice/HotelUiServiceApplication.java
+++ src/main/java/com/example/hoteluiservice/HotelUiServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.hoteluiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HotelUiServiceApplication {
 
     public static void main(String[] args) {
